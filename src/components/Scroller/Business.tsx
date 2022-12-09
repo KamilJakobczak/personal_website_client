e9257ea67c267d6f3d4d4e6b73c87b15afe49e29
Kamil Jakóbczak <k_j1@vp.pl>
2022-12-09 13:41:11 +0100

    add style for scroller starting with 280px width

--- src/components/Scroller/Business.tsx
+++ src/components/Scroller/Business.tsx
@@ -4,42 +4,51 @@ import { ReactComponent as MailIcon } from '../../images/mail_icon.svg';
 import SkillsSlider from './SkillsSlider';
 
 const Business: React.FC = () => {
-  const mainSkills = [
+  const mainSkills = ['React', 'Typescript', 'CSS (SASS)', 'HTML'];
+  const secondarySkills = [
     'Javascript',
-    'React',
-    'Typescript',
+    'GraphQL',
+    'Apollo',
+    'Redux',
+    'MongoDB',
+    'Prisma',
     'NodeJS',
-    'CSS (SASS)',
-    'HTML',
   ];
-  const secondarySkills = ['GraphQL', 'Apollo', 'Redux', 'MongoDB', 'Prisma'];
 
   return (
     <section className='scroller__business'>
       <h3>Skills and contact</h3>
       <SkillsSlider />
       <div className='scroller__business_skills'>
-        <ul className='skills__main'>
+        <div className='skills__main'>
           <h4>Main skills:</h4>
-          {mainSkills.map(skill => {
-            return <li key={skill}>{skill}</li>;
-          })}
-        </ul>
-        <ul className='skills__additional'>
-          <h4>Skills that I used in my biggest projects:</h4>
-          {secondarySkills.map(skill => {
-            return <li key={skill}>{skill}</li>;
-          })}
-        </ul>
-        <ul className='skills__recognized'>
-          <h4>Skills that I have basic knowledge of:</h4>
-          <li>Webpack</li>
-          <li>Bootstrap</li>
-        </ul>
-        <ul className='skills__in_progress'>
+          <ul>
+            {mainSkills.map(skill => {
+              return <li key={skill}>{skill}</li>;
+            })}
+          </ul>
+        </div>
+        <div className='skills__additional'>
+          <h4>I can use:</h4>
+          <ul>
+            {secondarySkills.map(skill => {
+              return <li key={skill}>{skill}</li>;
+            })}
+          </ul>
+        </div>
+        <div className='skills__recognized'>
+          <h4>Basic knowledge of:</h4>
+          <ul>
+            <li>Webpack</li>
+            <li>Bootstrap</li>
+          </ul>
+        </div>
+        <div className='skills__in_progress'>
           <h4>Skills I'm learning:</h4>
-          <li>React Native</li>
-        </ul>
+          <ul>
+            <li>React Native</li>
+          </ul>
+        </div>
       </div>
       <div className='scroller__business_contact'>
         <h5>send me a message!</h5>
