323ff73d02a0156a4dde999100826689956960fc
Kamil Jakóbczak <k_j1@vp.pl>
2023-02-06 10:09:39 +0100

    add expandable book filters

--- src/components/Projects/book_collection/BookList.tsx
+++ src/components/Projects/book_collection/BookList.tsx
@@ -1,17 +1,38 @@
 import { useQuery } from '@apollo/client';
+import { useState } from 'react';
 import { LOAD_BOOKS } from '../../../GraphQL/queries';
 import Error from '../../Error';
 import LoadingSpinner from '../../LoadingSpinner';
 import BookFilters from './BookFilters';
 import List from './List';
+import Button from './Button';
 
 const BookList: React.FC = () => {
   const { data, error, loading, refetch } = useQuery(LOAD_BOOKS);
+  const [filtersVisible, setFiltersVisible] = useState(false);
+
+  const handleFiltersClick = () => {
+    setFiltersVisible(prevState => !prevState);
+  };
 
   const showContent = () => {
     return (
       <>
-        <BookFilters refetchQuery={refetch} />
+        {filtersVisible ? (
+          <Button
+            className='book_collection__books__filter_showButton'
+            text='click to hide filters'
+            handleClick={handleFiltersClick}
+          />
+        ) : (
+          <Button
+            className='book_collection__books__filter_hideButton'
+            text='click to expand filters'
+            handleClick={handleFiltersClick}
+          />
+        )}
+        {filtersVisible ? <BookFilters refetchQuery={refetch} /> : null}
+
         {data && (
           <div className='book_collection__books_list'>
             <List data={data.books} />
