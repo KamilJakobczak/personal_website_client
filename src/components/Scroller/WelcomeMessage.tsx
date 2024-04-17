3450d4637f75c842c3c7dd04d7f52357e97c9469
Kamil Jakóbczak <k_j1@vp.pl>
2024-04-17 13:17:17 +0200

    adjust bio styling

--- src/components/Scroller/WelcomeMessage.tsx
+++ src/components/Scroller/WelcomeMessage.tsx
@@ -9,20 +9,26 @@ const WelcomeMessage: React.FC = () => {
             <img src={my_face} alt="jamar's face" />
           </div>
           <p>
-            I'm so happy that you decided to visit my page! I am a homegrown
+            I'm happy that you decided to visit my page! I am a homegrown
             front-end developer, using mostly React, but not exclusively.
           </p>
         </div>
-
+        <p>
+          I'm in my 30s and I have a small (2 + 1) family. Well, 2 + 2 if you
+          count the cat. I like reading books, going to cinema, playing
+          boardgames and some other 'nerdy' stuff.
+        </p>
         <p>
           I'm passionate about programming and I want to go as far as possible
           in this industry. For some reasons I haven't been able to pursue this
-          goal in the past but here I am now and I really enjoy what I'm doing!
+          goal but here I am now and I really enjoy what I'm doing!
         </p>
         <p>
           I'm hosting some of my projects on this page. Currently there are two
           of them: coding playground and book collection. You can check them out
-          in the next section.
+          in the next section. My next project will be some kind of a blog as I
+          sometimes feel the urge of letting out my thoughts on some topics. It
+          should be quite a learning process as well.
         </p>
         <p>
           Last section provides information about my skills and contact details.
